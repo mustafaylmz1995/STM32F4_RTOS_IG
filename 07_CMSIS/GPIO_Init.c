3bdfd15e7673a17bea80cfdab6cb0dceff5935ae
mustafaylmz1995 <mustafaylmz1995@gmail.com>
2022-03-14 22:17:41 +0300

    hata düzeltildi kod yükleme ayarları güncellendi

--- 07_CMSIS/GPIO_Init.c
+++ 07_CMSIS/GPIO_Init.c
@@ -15,9 +15,15 @@ void GPIO_Init(void){
   RCC->AHB1ENR  = RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOAEN;
   GPIOD->MODER  = (MODER_LED1 | MODER_LED2 | MODER_LED3 | MODER_LED4);
   
-  GPIOA->MODER  = MODER_SW1;
-  GPIOA->PUPDR  = PUM_SW1;
-  
+  GPIOA->MODER  |= MODER_SW1;
+  GPIOA->PUPDR  |= PDM_SW1;
+	
+	SYSCFG->EXTICR[1] = 0x00; //PA  
+	EXTI->IMR 				= EXTI_IMR_IM0;
+	EXTI->FTSR				=	EXTI_FTSR_TR0;
+	NVIC->ICPR
+	
+	
 }
 
 
@@ -81,44 +87,39 @@ void LED_Off(LEDs L){
     break;
   }
   
-  
 }
 
-void Blink(uint32_t L){
-  
-  LEDs Led = all;
-  
-  switch(L){
-    case 1:
-      Led = greenLED;
-    break;
-    case 2:
-      Led = orangeLED;
-    break;
-    case 3:
-      Led = redLED;
-    break;
-    case 4:
-      Led = blueLED;
-    break;
-    default:
-    break;
-  }
-  
-  LED_On(Led);
-  delay(1*ns2S);
-  LED_Off(Led);
-  delay(1*ns2S);
 
-  
-}
+
+/*
+To generate the interrupt, the interrupt line should be configured and enabled. This is done 
+by programming the two trigger registers with the desired edge detection and by enabling 
+the interrupt request by writing a ‘1’ to the corresponding bit in the interrupt mask register. 
+When the selected edge occurs on the external interrupt line, an interrupt request is 
+generated. The pending bit corresponding to the interrupt line is also set. This request is 
+reset by writing a ‘1’ in the pending register.
+*/
+/*
+To configure the 23 lines as interrupt sources, use the following procedure:
+• Configure the mask bits of the 23 interrupt lines (EXTI_IMR)
+• Configure the Trigger selection bits of the interrupt lines (EXTI_RTSR and EXTI_FTSR)
+• Configure the enable and mask bits that control the NVIC IRQ channel mapped to the 
+external interrupt controller (EXTI) so that an interrupt coming from one of the 23 lines 
+can be correctly acknowledged.
+*/
+
+/*
+eXTI0[3:0] BITS IN THE SYSCFG_exticr1 register
+
+*/
+
 
 
 uint32_t SW1_Input(void){
   static uint32_t val = 0;
   
   if( (GPIOA->IDR & SW1) ){
-    if(val == 3){
+    if(val == 5){
       val = 0;
     }else{
       val++;
--- 07_CMSIS/main.c
+++ 07_CMSIS/main.c
@@ -3,30 +3,10 @@
 #include "delay.h"
 
 
-//uint32_t sw_state;
-
-#define LED1        (1U<<12)	//PD12  greenLED
-#define LED2        (1U<<13)	//PD13  orangeLED
-#define LED3        (1U<<14)	//PD14  redLED
-#define LED4        (1U<<15)	//PD15  blueLED
-
-#define MODER_LED1  (1U<<24)
-#define MODER_LED2  (1U<<26)
-#define MODER_LED3  (1U<<28)
-#define MODER_LED4  (1U<<30)
-
-#define SW1         (1U<<0)	  //PA0 userSwitch
-#define MODER_SW1   (0U<<0) 
-#define PUM         (1U)      //Pull-Up mode
-#define PDM         (2U)      //Pull-Down mode
-#define PUM_SW1     (PUM<<0)
-#define PDM_SW1     (PDM<<0)
-
-
-
+uint32_t sw_state;
 
 int main(void){
-  /*
+
   GPIO_Init();
   
   while(1){
@@ -51,20 +31,7 @@ int main(void){
 		}
 		
 	}
-  */
-  
-  
-  
-  
-  RCC->AHB1ENR  = RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOAEN;
-  GPIOD->MODER  = (MODER_LED1 | MODER_LED2 | MODER_LED3 | MODER_LED4);
-  
-  GPIOA->MODER  = MODER_SW1;
-  GPIOA->PUPDR  = PUM_SW1;
-  
-  GPIOD->ODR |= LED1;
-  
-  
-  
+
+
 }
 
